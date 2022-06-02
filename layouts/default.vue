8d4ba518e080b47ed3d3740ddcbb8f80f22b3cf9
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-02 19:01:03 +0100

    add layout component

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <NavbarVue />
+    <div class="page-Container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarVue from "../components/Navbar.vue";
+
+export default {
+  components: {
+    NavbarVue,
+  },
+};
+</script>
+
+<style>
+.page-Container {
+  max-width: 40em;
+  margin: 0 auto;
+}
+</style>
