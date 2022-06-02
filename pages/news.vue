1aa24e9ce4f5ef45d690249420b44a7f88e34012
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-02 19:01:45 +0100

    add routes

---
+++ pages/news.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Here is the latest news</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+      consequuntur in incidunt repudiandae culpa, itaque, laborum minus modi hic
+      illum et harum atque saepe, repellat architecto reprehenderit! Quidem,
+      adipisci minima!
+    </p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "default",
+};
+</script>
