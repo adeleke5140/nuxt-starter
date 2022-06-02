b3b9e527fcd8c2a8a3fc89d5134f42168277c12f
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-06-02 19:01:22 +0100

    add components for the page

---
+++ components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Hello World!</h1>
+    </header>
+    <main>
+      <p>Welcome to my first vue app!</p>
+      <a href="/news"> Go to the news </a>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav">
+    <div class="logo">Logo</div>
+    <ul>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/news">News</NuxtLink>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarVue",
+};
+</script>
+
+<style scoped>
+.nav {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: rebeccapurple;
+}
+
+.logo {
+  border: 1px solid orange;
+  padding: 5px;
+}
+</style>
